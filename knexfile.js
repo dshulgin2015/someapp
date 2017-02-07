9d5b6f71fa54dd486fc3979cbb9f0950adf4361c
dshulgin2015 <unetby2016@yandex.ru>
2017-02-07 21:12:34 +0300

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,11 @@
-// Update with your config settings.
-require('dotenv').config({path: 'db.env'});
+
 
 
 module.exports = {
 
   development: {
 		client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://vqkwwouonqctvi:cee20663aaadc74235b240cdb5f7ad41e4d5b8c7218f71a86337d18b98fb25bb@ec2-54-243-38-139.compute-1.amazonaws.com:5432/d6uj4rs3kf0tg6',
     pool: {
       min: 2,
       max: 10

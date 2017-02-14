bb010d402f200927d9099b9f4e24319662f69d31
dshulgin2015 <dshulgin1@gmail.com>
2017-02-14 17:04:49 +0300

    '-'

--- routes/login.js
+++ routes/login.js
@@ -15,7 +15,9 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res, next) {
 
     passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err); }
+        if (err) {
+            console.log(err);
+            return next(err); }
         if (!user) { return res.redirect('/login'); }
         req.logIn(user, function(err) {
 
